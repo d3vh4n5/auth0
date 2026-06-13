6c99fc5cacef4ad2f5bb166ee01f8e4fbb41864e
jan <juanangelbasgall@hotmail.com>
2026-06-13 12:54:31 -0300

    variables move to env

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import crypto from "crypto";
+import { auth } from "express-oauth2-jwt-bearer";
+import { config } from "dotenv";
+
+config()
 
 const app = express();
 
-const CLIENT_ID = "k8yMEe7fBbHSKo51T5aNwpWIptJrQGcB";
-const ISSUER_BASE_URL = "https://dev-8jv8mok1bnikr1es.us.auth0.com";
-const REDIRECT_URI = "http://192.168.18.156:3000/auth/callback";
+const CLIENT_ID = process.env.CLIENT_ID;
+const ISSUER_BASE_URL = process.env.ISSUER_BASE_URL;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const AUDIENCE = process.env.AUDIENCE // Esta es la API a la que estoy pidiendo acceso, en Digito será la del cliente, Aquí será la misma solo para no tener 2 APIs distintas en este ejemplo
 
-const verifier = crypto.randomBytes(32).toString("base64url");
+// El codigo verificador debería guardarse en DB junto con el state.
+const verifiers = {}
 
 app.get("/login", (req, res) => {
+    const verifier = crypto.randomBytes(32).toString("base64url");
 
   const challenge = crypto
     .createHash("sha256")
@@ -18,11 +25,9 @@ app.get("/login", (req, res) => {
 
   const state = crypto.randomUUID();
 
-  console.log({
-    verifier,
-    challenge,
-    state
-  });
+  verifiers[`${state}`] = verifier // Guardaría el verifier asociado al state en un lugar seguro (DB)
+
+  console.log(verifiers)
 
   const url =
     `${ISSUER_BASE_URL}/authorize?` +
@@ -30,13 +35,13 @@ app.get("/login", (req, res) => {
       response_type: "code",
       client_id: CLIENT_ID,
       redirect_uri: REDIRECT_URI, // La API Nuestra a la que el cliente va a redirigir después de autenticarse
-      audience: "https://tm6l5mqj-3000.brs.devtunnels.ms", // La api a la qu eestoy pidiendo permisos (La del cliente en este caso)
+      audience: AUDIENCE, // La api a la qu eestoy pidiendo permisos (La del cliente en este caso)
       scope: "openid profile email offline_access",
       state,
       code_challenge: challenge,
       code_challenge_method: "S256",
     });
-    console.log("Redirecting to:", url);
+    console.log("Resulting url:", url);
   return res.send(url);
   res.redirect(url);
 });
@@ -44,8 +49,19 @@ app.get("/login", (req, res) => {
 app.get("/auth/callback", async (req, res) => {
   const { code, state } = req.query;
 
+  const verifier = verifiers[state]
+
   console.log("Received code:", code);
   console.log("Received state:", state);
+  console.log("Found verifier:", verifier);
+
+  if (!verifier) 
+    // Template de verificación fallida
+    return res.status(401).json(
+    {
+        error: "No verifier found for this state"
+    }
+  )
 
   // In a real application, you would verify the state and retrieve the verifier from a secure store
 
@@ -68,8 +84,9 @@ app.get("/auth/callback", async (req, res) => {
 
     const tokens = await response.json();
 
-    console.log(tokens, verifier);
+    console.log({response})
 
+    // Template de verificación exitosa
     res.json({
         verifier,
         message: "Authentication successful",
@@ -77,9 +94,16 @@ app.get("/auth/callback", async (req, res) => {
     });
 });
 
-app.get("/api", (req, res) => {
+// Esto en realidad, en el caso de Digito, sería la API del cliente, no estaría de nuestro lado
+const checkJwt = auth({
+  audience: AUDIENCE,
+  issuerBaseURL: ISSUER_BASE_URL,
+});
+
+app.get("/api", checkJwt, (req, res) => {
   res.json({
-    message: "Hello from the API!",
+    status: "OK",
+    message: "Token válido: Accediendo a información protegida",
   });
 });
 
